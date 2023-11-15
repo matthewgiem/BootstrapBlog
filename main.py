a7365b24ee25d688da5b2cf3ffeeba12422b3728
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-11-15 18:17:35 +0700

    use jinja to add hrefs for the header

--- main.py
+++ main.py
@@ -4,8 +4,24 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def get_all_posts():
-    return render_template("index.html")
+def home():
+    header_title = "Home"
+    return render_template("index.html", title=header_title)
+
+@app.route('/about')
+def about():
+    header_title = "About"
+    return render_template("about.html", title=header_title)
+
+@app.route('/post')
+def post():
+    header_title = "Post"
+    return render_template("post.html", title=header_title)
+
+@app.route('/contact')
+def contact():
+    header_title = "Contact"
+    return render_template("contact.html", title=header_title)
 
 if __name__ == "__main__":
     app.run(debug=True)
